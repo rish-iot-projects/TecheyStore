c4db7ad6a0742ee2c7e036bbe0ea4fd016dfa4a2
rish-iot-projects <rishabhsinha2001@yahoo.com>
2025-04-12 14:51:16 -0400

    Update app.module.ts

--- server/client/src/app/app.module.ts
+++ server/client/src/app/app.module.ts
@@ -1,1 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { OrderComponent } from './order/order.component';
 
+@NgModule({
+  declarations: [AppComponent, HomeComponent, ProductsComponent, OrderComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
